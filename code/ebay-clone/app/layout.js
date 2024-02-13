2dad2da11c824f478474cd9e482e5122b05bd575
Steven Zhao <stevenzhao456@gmail.com>
2024-02-12 18:11:19 -0600

    Removed template CSS code, added Main Layout Page

--- code/ebay-clone/app/layout.js
+++ code/ebay-clone/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const metadata = {
   title: "Ebay Clone",
---
+++ code/ebay-clone/app/layouts/MainLayout.js
@@ -0,0 +1,10 @@
+"use client";
+
+// Main Layout Content
+export default function MainLayout({ children }) {
+  return (
+    <>
+      <div id="Mainlayout">ML</div>
+    </>
+  );
+}
