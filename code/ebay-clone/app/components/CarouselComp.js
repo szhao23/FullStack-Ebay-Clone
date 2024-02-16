bb469ab8482044934477c04a58402369483c7b4f
Steven Zhao <stevenzhao456@gmail.com>
2024-02-15 23:14:13 -0600

    Finished Carousel component for now

--- code/ebay-clone/app/components/CarouselComp.js
+++ code/ebay-clone/app/components/CarouselComp.js
@@ -3,7 +3,9 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Image from "next/image";
-import banner from "../../public/images/banner/1.png";
+import banner1 from "../../public/images/banner/1.png";
+import banner2 from "../../public/images/banner/2.png";
+import banner3 from "../../public/images/banner/3.png";
 
 export default function CarouselComp() {
   return (
@@ -16,7 +18,9 @@ export default function CarouselComp() {
           infiniteLoop={true}
           showThumbs={false}
         >
-          <Image width={594} src={banner} unoptimized />
+          <Image width={594} src={banner1} unoptimized />
+          <Image width={594} src={banner2} unoptimized />
+          <Image width={594} src={banner3} unoptimized />
         </Carousel>
       </div>
     </>
