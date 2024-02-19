b579fda05cb0f30c95e8e1deb29151ed0783e19d
Steven Zhao <stevenzhao456@gmail.com>
2024-02-19 02:00:09 -0600

    Sign out function added

--- code/ebay-clone/app/layouts/includes/TopMenu.js
+++ code/ebay-clone/app/layouts/includes/TopMenu.js
@@ -65,7 +65,13 @@ export default function TopMenu() {
                 <li className="text-[11px] py-2 px-4 w-full hover:underline text-blue-500 hover:text-blue-600 cursor-pointer">
                   <Link href="/orders">My Orders</Link>
                 </li>
-                <li className="text-[11px] py-2 px-4 w-full hover:underline text-blue-500 hover:text-blue-600 cursor-pointer">
+                <li
+                  onClick={() => {
+                    user.signOut();
+                    setIsMenu(false);
+                  }}
+                  className="text-[11px] py-2 px-4 w-full hover:underline text-blue-500 hover:text-blue-600 cursor-pointer"
+                >
                   Sign Out
                 </li>
               </ul>
