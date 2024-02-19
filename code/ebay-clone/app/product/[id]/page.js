897c88202921bc3e3b496a7174ff363bf73956b9
Steven Zhao <stevenzhao456@gmail.com>
2024-02-18 22:54:18 -0600

    Added Condition, separator, and Price styling for Product Details page

--- code/ebay-clone/app/product/[id]/page.js
+++ code/ebay-clone/app/product/[id]/page.js
@@ -2,7 +2,6 @@
 
 import MainLayout from "@/app/layouts/MainLayout";
 
-
 export default function Product({ params }) {
   const product = {
     id: 1,
@@ -18,11 +17,7 @@ export default function Product({ params }) {
       <div className="max-w-[1200px] mx-auto">
         <div className="flex px-4 py-10">
           {product?.url ? (
-            <img
-              
-              className="w-[40%] rounded-lg"
-              src={product?.url + "/280"}
-            />
+            <img className="w-[40%] rounded-lg" src={product?.url + "/280"} />
           ) : (
             <div className="w-[40%]"></div>
           )}
@@ -30,6 +25,30 @@ export default function Product({ params }) {
             <div className="font-bold text-xl">{product?.title}</div>
             <div className="text-sm text-gray-700 pt-2">
               Brand New - Full Waranty
+            </div>
+
+            <div className="border-b py-1" />
+
+            <div className="pt-3 pb-2">
+              <div className="flex items-center">
+                Condition:{" "}
+                <span className="font-bold text-[17px] ml-2">New</span>
+              </div>
+            </div>
+
+            <div className="border-b py-1" />
+
+            <div className="pt-3">
+              <div className="w-full flex items-center justify-between">
+                <div className="flex items-center">
+                  Price:
+                  {product?.price ? (
+                    <div className="font-bold text-[20px] ml-2">
+                      USD ${(product?.price / 100).toFixed(2)}
+                    </div>
+                  ) : null}
+                </div>
+              </div>
             </div>
           </div>
         </div>
