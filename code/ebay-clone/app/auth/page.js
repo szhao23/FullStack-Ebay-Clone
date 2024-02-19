fc9f8384db84ab8c055ac0793fb3e753357fb33a
Steven Zhao <stevenzhao456@gmail.com>
2024-02-19 00:29:23 -0600

    Added Address page and styling

--- code/ebay-clone/app/auth/page.js
+++ code/ebay-clone/app/auth/page.js
@@ -1,10 +1,13 @@
 "use client";
 
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 import Link from "next/link";
 
 export default function AuthPage() {
+  // const supabase = createClientComponentClient();
+
   return (
     <>
       <div id="AuthPage" className="w-full min-h-screen bg-white">
@@ -19,11 +22,13 @@ export default function AuthPage() {
         </div>
 
         <div className="max-w-[400px] mx-auto px-2">
-            <Auth 
-                onlyThirdPartyProviders
-                providers={['google']}
-                appearance={{theme: ThemeSupa}}
-            />
+          {/* <Auth
+            onlyThirdPartyProviders
+            redirectTo={`${window.location.origin}/auth/callback`}
+            supabaseClient={supabase}
+            providers={["google"]}
+            appearance={{ theme: ThemeSupa }}
+          /> */}
         </div>
       </div>
     </>
