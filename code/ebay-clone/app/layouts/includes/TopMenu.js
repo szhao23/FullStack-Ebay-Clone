79e9d448b92be26b2350c52b898215c8232a0905
Steven Zhao <stevenzhao456@gmail.com>
2024-02-19 01:58:46 -0600

    Added user sign in and sign out functionality, menu will show if user is signed in, else won't show if not authenticated

--- code/ebay-clone/app/layouts/includes/TopMenu.js
+++ code/ebay-clone/app/layouts/includes/TopMenu.js
@@ -3,8 +3,39 @@
 import Link from "next/link";
 import { BsChevronDown } from "react-icons/bs";
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import { useUser } from "@/app/context/user";
+import { useState } from "react";
 
 export default function TopMenu() {
+  const user = useUser();
+
+  const [isMenu, setIsMenu] = useState(false);
+
+  const isLoggedIn = () => {
+    if (user && user?.id) {
+      return (
+        <button
+          onClick={() => (!isMenu ? setIsMenu(true) : setIsMenu(false))}
+          className="flex items-center gap-2 hover:underline cursor-pointer"
+        >
+          <div>Hi, {user.name}</div>
+          <BsChevronDown />
+        </button>
+      );
+    }
+
+    // If the above does not trigger, return the below
+    return (
+      <Link
+        href="/auth"
+        className="flex items-center gap-2 hover:underline cursor-pointer"
+      >
+        <div>Login</div>
+        <BsChevronDown />
+      </Link>
+    );
+  };
+
   return (
     <div id="TopMenu" className="border-b">
       <div className="flex items-center justify-between w-full mx-auto max-w-[1200px]">
@@ -14,21 +45,18 @@ export default function TopMenu() {
           className="flex items-center text-[11px] text-[#333333] px-2 h-8"
         >
           <li className="relative px-3">
-            <Link
-              href="/auth"
-              className="flex items-center gap-2 hover:underline cursor-pointer"
-            >
-              <div>Login</div>
-              <BsChevronDown />
-            </Link>
+            {isLoggedIn()}
             <div
               // Dropdown bar bottom
               id="AuthDropdown"
-              className="hidden absolute bg-white w-[200px] text-[#333333] z-40 top-[20px] left-0 border shadow-lg"
+              className={`
+                 absolute bg-white w-[200px] text-[#333333] z-40 top-[20px] left-0 border shadow-lg
+                 ${isMenu ? "visible" : "hidden"}
+              `}
             >
               <div className="flex items-center justify-start gap-1 p-3">
-                <img width={50} src="https://picsum.photos/200/" />
-                <div className="font-bold text-[13px]">Hello there</div>
+                <img width={50} src={user?.picture} />
+                <div className="font-bold text-[13px]">{user?.name}</div>
               </div>
 
               <div className="border-b" />
