f23a4197069e509e35eddbbd8990b93ce5b58450
Steven Zhao <stevenzhao456@gmail.com>
2024-03-01 05:22:30 -0600

    Added API endpoint for GET request for orders

---
+++ code/ebay-clone/app/api/address/orders/route.js
@@ -0,0 +1,39 @@
+import prsima from "@/app/libs/prisma";
+import { NextResponse } from "next/server";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+// Update Method without being logged in
+export async function GET() {
+  const supabase = createServerComponentClient({ cookies });
+
+  try {
+    // Try to get the current active user
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    // If no active user, throw error
+    if (!user) throw Error();
+
+    // Get all of the order items associated with an order
+    const orders = await prisma.orders.findMany({
+      where: { user_id: user?.id },
+      orderBy: { id: "desc" },
+      include: {
+        orderItem: {
+          include: {
+            product: true,
+          },
+        },
+      },
+    });
+
+    await prisma.$disconnect();
+    return new NextResponse.json(orders);
+  } catch (error) {
+    console.log(error);
+    await prisma.$disconnect();
+    return new NextResponse("Uh oh, Something went wrong...", { status: 400 });
+  }
+}
