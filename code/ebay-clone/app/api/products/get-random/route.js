bd75e078c5393f068e5e1acf637eed6bceef4433
Steven Zhao <stevenzhao456@gmail.com>
2024-03-18 14:06:39 -0500

    Added get-random API folder for fetching products, counting products, and fetching randomly

---
+++ code/ebay-clone/app/api/products/get-random/route.js
@@ -0,0 +1,27 @@
+import prsima from "@/app/libs/prisma";
+import { NextResponse } from "next/server";
+
+// GET Method
+export async function GET() {
+  try {
+    // Get ALL products and count them
+    const productsCount = await prisma.products.count();
+    const skip = Math.floor(Math.random() * productsCount);
+
+    const products = await prisma.products.findMany({
+      take: 5,
+      skip: skip,
+      orderBy: { id: "asc" },
+    });
+
+    // Get items
+
+    await prisma.$disconnect();
+    // Return items
+    return NextResponse.json(products);
+  } catch (error) {
+    console.log(error);
+    await prisma.$disconnect();
+    return new NextResponse("Uh oh, Something went wrong...", { status: 400 });
+  }
+}
