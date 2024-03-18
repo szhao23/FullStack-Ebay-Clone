8a39f432737ff3ed02b370a500fc4eaf5e742b79
Steven Zhao <stevenzhao456@gmail.com>
2024-03-18 14:04:10 -0500

    Added API folder search-by-name for using search filter queries

---
+++ code/ebay-clone/app/api/products/search-by-name/route.js
@@ -0,0 +1,29 @@
+import prsima from "@/app/libs/prisma";
+import { NextResponse } from "next/server";
+
+// GET Method
+export async function GET(req, context) {
+  try {
+    // Get name from params because we will be using the name to search
+    const { name } = context.params;
+
+    // Get items
+    const items = await prisma.products.findMany({
+      take: 5,
+      where: {
+        title: {
+          contains: name,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    await prisma.$disconnect();
+    // Return items
+    return NextResponse.json(items);
+  } catch (error) {
+    console.log(error);
+    await prisma.$disconnect();
+    return new NextResponse("Uh oh, Something went wrong...", { status: 400 });
+  }
+}
